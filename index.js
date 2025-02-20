7442c9a3dbd19fe1b87ac913e3250d552800b424
Hazrat Bilal <hazratbilal52521@gmail.com>
2025-02-20 11:02:15 +0500

    cors allow

--- index.js
+++ index.js
@@ -15,6 +15,21 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const allowedOrigins = ["http://localhost:3000"];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
+
 // Routes
 app.use("/api/auth", authRouter);
 app.use("/api", skRouter);
