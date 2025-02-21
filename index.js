79de88a8b92d7cc7f7ca2986b22bb6fc1821c84c
Hazrat Bilal <hazratbilal52521@gmail.com>
2025-02-21 11:40:19 +0500

    ..

--- index.js
+++ index.js
@@ -12,23 +12,13 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
-app.use(express.json());
-
-const allowedOrigins = ["http://localhost:3000"];
-
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
+    origin: ["http://localhost:3000"], // Allow requests from the React app
+    credentials: true, // Allow cookies and credentials
   })
 );
+app.use(express.json());
 
 // Routes
 app.use("/api/auth", authRouter);
@@ -44,5 +34,3 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
-
