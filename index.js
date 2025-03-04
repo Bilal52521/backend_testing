0fd6e5722fb4ebac75635c91fac4504b81bcd2bb
Hazrat Bilal <hazratbilal52521@gmail.com>
2025-03-04 23:14:43 +0500

    .

--- index.js
+++ index.js
@@ -5,17 +5,17 @@ import express from "express";
 // import skRouter from "./routes/sk.route.js";
 // import wtRouter from "./routes/wk.route.js";
 
-dotenv.config();
+// dotenv.config();
 
 const app = express();
 
 // Middleware
-app.use(
-  cors({
-    origin: ["http://localhost:3000"],
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: ["http://localhost:3000"],
+//     credentials: true,
+//   })
+// );
 app.use(express.json());
 
 // Routes
