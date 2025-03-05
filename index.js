1f990b818c305f4a285bd50e63cb896dd95f9757
Hazrat Bilal <hazratbilal52521@gmail.com>
2025-03-05 14:03:25 +0500

    ...

--- index.js
+++ index.js
@@ -1,38 +1,9 @@
-import express from "express";
-// import cors from "cors";
-// import dotenv from "dotenv";
-// import authRouter from "./routes/auth.route.js";
-// import skRouter from "./routes/sk.route.js";
-// import wtRouter from "./routes/wk.route.js";
-
-// dotenv.config();
-
-const PORT = process.env.PORT || 5000;
+const express = require("express");
 const app = express();
-app.use(express.json());
 
-// Middleware
-// app.use(
-//   cors({
-//     origin: ["http://localhost:3000"],
-//     credentials: true,
-//   })
-// );
-
-// Routes
-// app.use("/api/auth", authRouter);
-// app.use("/api", skRouter);
-// app.use("/api", wtRouter);
-
-// Test Routes
-app.get("/test", (req, res) => {
-  res.send("Testing API working...");
-});
 
 app.get("/", (req, res) => {
-  res.send("Default Testing API working...");
+  res.send("Hello World!");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+export default app;
--- server.js
+++ server.js
@@ -5,3 +5,47 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
+
+
+
+
+// import express from "express";
+// import cors from "cors";
+// import dotenv from "dotenv";
+// import authRouter from "./routes/auth.route.js";
+// import skRouter from "./routes/sk.route.js";
+// import wtRouter from "./routes/wk.route.js";
+
+// dotenv.config();
+
+// const PORT = process.env.PORT || 5000;
+// const app = express();
+// app.use(express.json());
+
+// // Middleware
+// app.use(
+//   cors({
+//     origin: ["http://localhost:3000"],
+//     credentials: true,
+//   })
+// );
+
+// // Routes
+// app.use("/api/auth", authRouter);
+// app.use("/api", skRouter);
+// app.use("/api", wtRouter);
+
+// // Test Routes
+// app.get("/test", (req, res) => {
+//   res.send("Testing API working...");
+// });
+
+// app.get("/", (req, res) => {
+//   res.send("Default Testing API working...");
+// });
+
+// app.listen(PORT, () => {
+//   console.log(`Server is running on http://localhost:${PORT}`);
+// });
+
+// export default app;
