14821901a022d0bbffa99d9ddd351f07fa942259
Hazrat Bilal <hazratbilal52521@gmail.com>
2025-03-05 14:08:23 +0500

    zzz

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import express from "express";
+const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-export default app; 
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
