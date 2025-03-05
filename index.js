7da5dbc613d5c04b3bb996b45a915353591f3467
Hazrat Bilal <hazratbilal52521@gmail.com>
2025-03-05 15:26:23 +0500

    update index.js

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-import authRouter from "./routes/auth.route.js";
-import skRouter from "./routes/sk.route.js";
-import wtRouter from "./routes/wk.route.js";
+// import cors from "cors";
+// import dotenv from "dotenv";
+// import authRouter from "./routes/auth.route.js";
+// import skRouter from "./routes/sk.route.js";
+// import wtRouter from "./routes/wk.route.js";
 
-dotenv.config();
+// dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(express.json());
 
-// Middleware
-app.use(
-  cors({
-    origin: ["http://localhost:3000"],
-    credentials: true,
-  })
-);
+// // Middleware
+// app.use(
+//   cors({
+//     origin: ["http://localhost:3000"],
+//     credentials: true,
+//   })
+// );
 
-// Routes
-app.use("/api/auth", authRouter);
-app.use("/api", skRouter);
-app.use("/api", wtRouter);
+// // Routes
+// app.use("/api/auth", authRouter);
+// app.use("/api", skRouter);
+// app.use("/api", wtRouter);
 
 // Test Routes
 app.get("/test", (req, res) => {
