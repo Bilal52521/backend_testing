587a789c534e90ad1d381a0236697fd9a6e2545e
Hazrat Bilal <hazratbilal52521@gmail.com>
2025-03-05 16:02:46 +0500

    `````

--- config/db.js
+++ config/db.js
@@ -1,7 +1,8 @@
 import { Sequelize } from "sequelize";
+import mysql2 from "mysql2"; // Ensure mysql2 is required
 import dotenv from "dotenv";
 
-dotenv.config(); // Load environment variables
+dotenv.config();
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
@@ -10,6 +11,7 @@ const sequelize = new Sequelize(
   {
     host: process.env.DB_HOST,
     dialect: "mysql",
+    dialectModule: mysql2, // Specify mysql2 explicitly
     logging: false,
   }
 );
