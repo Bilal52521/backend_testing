42e56a5acfa522df54988b5e03b26c7bc90c68d5
Hazrat Bilal <hazratbilal52521@gmail.com>
2025-03-05 15:17:02 +0500

    aaqq

--- handler.js
+++
@@ -1,5 +0,0 @@
-import app from "./api/index.js";
-import { createServer } from "@vercel/node";
-
-// Convert Express app into a serverless function
-export default createServer(app);
--- index.js
+++ index.js
@@ -1,9 +1,21 @@
-import app from "./api/index.js";
-
+import express from "express";
 const PORT = process.env.PORT || 5000;
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
+
+// import app from "./api/index.js";
+
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//   console.log(`Server running at http://localhost:${PORT}`);
+// });
 
 // import express from "express";
 // import cors from "cors";
